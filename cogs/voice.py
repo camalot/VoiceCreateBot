a065b6548399b8bd88c25cb6f6448d3dd8fb36cd
Ryan Conrad <camalot@gmail.com>
2019-12-14 09:18:12 -0600

    fix syntax

--- cogs/voice.py
+++ cogs/voice.py
@@ -44,12 +44,30 @@ class voice(commands.Cog):
         self.admin_ids = os.environ["ADMIN_USERS"].split(" ")
         self.initDB()
 
+    # Clean up empty channels
     async def clean_up_channels(self, guild):
         conn = sqlite3.connect(self.db_path)
         c = conn.cursor()
-        guildID = member.guild.id
+        guildID = guild.id
         c.execute("SELECT voiceChannelID FROM guild WHERE guildID = ?", (guildID,))
         voiceChannels = [item for clist in c.fetchall() for item in clist]
+        for chanID in known_channels:
+            chan = guild.get_channel(chanID)
+            if not chan or len(chan.members) == 0:
+                print(f"Delete orphan voice channel '{chan.name}'")
+                await chan.delete()
+                c.execute("SELECT channelID FROM textChannel WHERE userID = ? AND guildID = ? AND voiceID = ?", (aid, guildID, channelID))
+                textGroup = c.fetchone()
+                textChannel = None
+                if textGroup is not None:
+                    textChannel = self.bot.get_channel(textGroup[0])
+                if textChannel is not None:
+                    print(f"Delete orphan text channel '{textChannel.name}'")
+                    await textChannel.delete()
+            c.execute("DELETE FROM voiceChannel WHERE guildID = ? AND voiceID = ?", (guildID, chanID, ))
+            c.execute("DELETE FROM textChannel WHERE guildID = ? AND voiceID = ?", (guildID, chanID, ))
+        conn.commit()
+
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -63,33 +81,13 @@ class voice(commands.Cog):
             pass
         else:
             try:
-                # get empty channels
-                c.execute("SELECT voiceID from voiceChannel WHERE guildID = ?", (guildID,))
-                known_channels = [item for chans in c.fetchall() for item in chans]
-                for chanID in known_channels:
-                    chan = member.guild.get_channel(chanID)
-                    if not chan or len(chan.members) == 0:
-                        if chan:
-                            print(f"Delete orphan voice channel '{chan.name}'")
-                            await chan.delete()
-                            c.execute("SELECT channelID FROM textChannel WHERE userID = ? AND guildID = ? AND voiceID = ?", (aid, guildID, channelID))
-                            textGroup = c.fetchone()
-                            textChannel = None
-                            if textGroup is not None:
-                                textChannel = self.bot.get_channel(textGroup[0])
-                            if textChannel is not None:
-                                print(f"Delete orphan text channel '{textChannel.name}'")
-                                await textChannel.delete()
-                        c.execute("DELETE FROM voiceChannel WHERE guildID = ? AND voiceID = ?", (guildID, chanID, ))
-                        c.execute("DELETE FROM textChannel WHERE guildID = ? AND voiceID = ?", (guildID, chanID, ))
-                conn.commit()
+                await self.clean_up_channels(member.guild)
 
                 if after.channel is not None and after.channel.id in voiceChannels:
                     category_id = after.channel.category_id
                     c.execute("SELECT channelName, channelLimit FROM userSettings WHERE userID = ?", (member.id,))
                     setting = c.fetchone()
-                    c.execute(
-                        "SELECT channelLimit, channelLocked FROM guildCategorySettings WHERE guildID = ? and voiceCategoryID = ?", (guildID, category_id,))
+                    c.execute("SELECT channelLimit, channelLocked FROM guildCategorySettings WHERE guildID = ? and voiceCategoryID = ?", (guildID, category_id,))
                     guildSetting = c.fetchone()
                     if setting is None:
                         name = f"{member.name}'s channel"
@@ -188,7 +186,7 @@ class voice(commands.Cog):
                     "name": "Example",
                     "value": f"{prefix}voice name CoD:MW US"
                 })
-                await self.sendEmbed(ctx, "Voice Channel Prefix", f'Command prefix is now "{prefix}".' delete_after=60)
+                await self.sendEmbed(ctx, "Voice Channel Prefix", f'Command prefix is now "{prefix}".', delete_after=60)
 
     @voice.command()
     async def help(self, ctx, command=""):
