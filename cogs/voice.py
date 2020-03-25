67a3b1ccacac7b9f3b6689803f4c92c6b2b24312
Ryan Conrad <camalot@gmail.com>
2020-03-25 10:46:38 -0500

    add admin command to rename a channel

--- cogs/voice.py
+++ cogs/voice.py
@@ -592,39 +592,83 @@ class voice(commands.Cog):
 
     @voice.command()
     async def name(self, ctx, *, name):
-        conn = sqlite3.connect(self.db_path)
-        c = conn.cursor()
-        aid = ctx.author.id
-        guildID = ctx.guild.id
-        c.execute("SELECT voiceID FROM voiceChannel WHERE userID = ? AND guildID = ?", (aid, guildID,))
-        voiceGroup = c.fetchone()
-        if voiceGroup is None:
-            await self.sendEmbed(ctx, "Updated Channel Name", f"{ctx.author.mention} You don't own a channel.", delete_after=5)
-        else:
-            channelID = voiceGroup[0]
-            c.execute("SELECT channelID FROM textChannel WHERE guildID = ? AND voiceID = ?", (guildID, channelID))
-            textGroup = c.fetchone()
-            textChannel = None
-            channel = self.bot.get_channel(channelID)
-            if channel is not None:
+        try:
+            conn = sqlite3.connect(self.db_path)
+            c = conn.cursor()
+            aid = ctx.author.id
+            guildID = ctx.guild.id
+            c.execute("SELECT voiceID FROM voiceChannel WHERE userID = ? AND guildID = ?", (aid, guildID,))
+            voiceGroup = c.fetchone()
+            if voiceGroup is None:
+                await self.sendEmbed(ctx, "Updated Channel Name", f"{ctx.author.mention} You don't own a channel.", delete_after=5)
+            else:
+                channelID = voiceGroup[0]
+                c.execute("SELECT channelID FROM textChannel WHERE guildID = ? AND voiceID = ?", (guildID, channelID))
+                textGroup = c.fetchone()
+                textChannel = None
+                channel = self.bot.get_channel(channelID)
+                if channel is not None:
 
-                if textGroup is not None:
-                    textChannel = self.bot.get_channel(textGroup[0])
-                if textChannel is not None:
-                    await textChannel.edit(name=name)
+                    if textGroup is not None:
+                        textChannel = self.bot.get_channel(textGroup[0])
+                    if textChannel is not None:
+                        await textChannel.edit(name=name)
 
-                await channel.edit(name=name)
-                await self.sendEmbed(ctx, "Updated Channel Name", f'You have changed the channel name to {name}!', delete_after=5)
-                c.execute("SELECT channelName FROM userSettings WHERE userID = ? AND guildID = ?", (aid, guildID,))
-                voiceGroup = c.fetchone()
-                if voiceGroup is None:
-                    c.execute("INSERT INTO userSettings VALUES (?, ?, ?, ?)", (guildID, aid, name, 0))
-                else:
-                    c.execute("UPDATE userSettings SET channelName = ? WHERE userID = ? AND guildID = ?", (name, aid, guildID,))
+                    await channel.edit(name=name)
+                    await self.sendEmbed(ctx, "Updated Channel Name", f'You have changed the channel name to {name}!', delete_after=5)
+                    c.execute("SELECT channelName FROM userSettings WHERE userID = ? AND guildID = ?", (aid, guildID,))
+                    voiceGroup = c.fetchone()
+                    if voiceGroup is None:
+                        c.execute("INSERT INTO userSettings VALUES (?, ?, ?, ?)", (guildID, aid, name, 0))
+                    else:
+                        c.execute("UPDATE userSettings SET channelName = ? WHERE userID = ? AND guildID = ?", (name, aid, guildID,))
+            conn.commit()
+            conn.close()
+        except Exception as ex:
+            print(ex)
+            traceback.print_exc()
+        finally:
+            await ctx.message.delete()
 
-        await ctx.message.delete()
-        conn.commit()
-        conn.close()
+
+    @voice.command(aliases=["rename"])
+    async def force_name(self, ctx, *, name):
+        try:
+            conn = sqlite3.connect(self.db_path)
+            c = conn.cursor()
+            aid = ctx.author.id
+            guildID = ctx.guild.id
+            if self.isAdmin(ctx):
+                channelID = voiceGroup[0]
+                c.execute("SELECT channelID FROM textChannel WHERE guildID = ? AND voiceID = ?", (guildID, channelID))
+                textGroup = c.fetchone()
+                textChannel = None
+                channel = self.bot.get_channel(channelID)
+                if channel is not None:
+
+                    c.execute("SELECT userID FROM voiceChannel WHERE voiceID = ? AND guildID = ?", (channelID, guildID,))
+                    channelOwner = c.fetchone()
+
+                    if textGroup is not None:
+                        textChannel = self.bot.get_channel(textGroup[0])
+                    if textChannel is not None:
+                        await textChannel.edit(name=name)
+
+                    await channel.edit(name=name)
+                    await self.sendEmbed(ctx, "Updated Channel Name", f'You have changed the channel name to {name}!', delete_after=5)
+                    c.execute("SELECT channelName FROM userSettings WHERE userID = ? AND guildID = ?", (channelOwner, guildID,))
+                    voiceGroup = c.fetchone()
+                    if voiceGroup is None:
+                        c.execute("INSERT INTO userSettings VALUES (?, ?, ?, ?)", (guildID, channelOwner, name, 0))
+                    else:
+                        c.execute("UPDATE userSettings SET channelName = ? WHERE userID = ? AND guildID = ?", (name, channelOwner, guildID,))
+            else:
+                print(f"{ctx.author} tried to run command 'rename'")
+        except Exception as ex:
+            print(ex)
+            traceback.print_exc()
+        finally:
+            await ctx.message.delete()
 
     @voice.command()
     async def whoowns(self, ctx):
