0c31f366db7aa773f06d943cb39db591e72c388d
Ryan Conrad <camalot@gmail.com>
2021-08-28 10:27:46 -0500

    update the name function with data from github gist

--- bot/cogs/lib/utils.py
+++ bot/cogs/lib/utils.py
@@ -7,6 +7,8 @@ import os
 import glob
 import typing
 import requests
+import random
+import re
 
 def dict_get(dictionary, key, default_value = None):
     if key in dictionary.keys():
@@ -34,13 +36,42 @@ def chunk_list(lst, size):
 
 def get_random_name(noun_count = 1, adjective_count = 1):
     try:
-        nouns = requests.get(f"https://random-word-form.herokuapp.com/random/noun?count={str(noun_count)}").json()
-        adjectives = requests.get(f"https://random-word-form.herokuapp.com/random/adjective?count={str(adjective_count)}").json()
+        adjectives = load_from_gist("adjectives", adjective_count)
+        nouns = load_from_gist("nouns", noun_count)
         results = adjectives + nouns
+        print("FROM GIST")
         return " ".join(w.title() for w in results)
-    except:
+    except Exception as ex:
+        print(ex)
+        traceback.print_exc()
         try:
-            results = requests.get(f"https://random-word-api.herokuapp.com/word?number={str(noun_count + adjective_count)}&swear=0").json()
+            nouns = requests.get(f"https://random-word-form.herokuapp.com/random/noun?count={str(noun_count)}").json()
+            adjectives = requests.get(f"https://random-word-form.herokuapp.com/random/adjective?count={str(adjective_count)}").json()
+            results = adjectives + nouns
+            print("FROM random-word-form")
             return " ".join(w.title() for w in results)
-        except:
-            return "New Voice Channel"
+        except Exception as ex:
+            print(ex)
+            traceback.print_exc()
+            try:
+                print("FROM random-word-api")
+                results = requests.get(f"https://random-word-api.herokuapp.com/word?number={str(noun_count + adjective_count)}&swear=0").json()
+                return " ".join(w.title() for w in results)
+            except Exception as ex:
+                print(ex)
+                traceback.print_exc()
+                return "New Voice Channel"
+
+
+def load_from_gist(type, count):
+    types = [ "adjectives", "nouns", "verbs" ]
+    if type not in types:
+        type = "nouns"
+    if count <= 0:
+        count = 1
+    elif count > 10:
+        count = 10
+    data = requests.get(f"https://gist.githubusercontent.com/ijmacdowell/8325491/raw/b39de3a6ba03205380caf5d58e0cae8a869ac36d/{type}.js").text
+    data = re.sub(r"(var\s(adjectives|nouns|verbs)\s=\s)|;$","", data)
+    jdata = json.loads(data)
+    return random.sample(jdata, count)
