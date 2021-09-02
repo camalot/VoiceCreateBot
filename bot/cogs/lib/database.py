b5cc530415bcd1b8c18ef45f5bd515064eb5eea3
Ryan Conrad <camalot@gmail.com>
2021-09-02 16:28:31 -0500

    implement missing functions

--- bot/cogs/lib/database.py
+++ bot/cogs/lib/database.py
@@ -20,6 +20,10 @@ class Database():
 
     def clean_tracked_channels(self, guildId, voiceChannelId, textChannelId):
         pass
+    def clean_guild_user_settings(self, guildId):
+        pass
+    def clean_user_settings(self, guildId, userId):
+        pass
     def get_tracked_voice_channel_id_by_owner(self, guildId, ownerId):
         pass
     def get_channel_owner_id(self, guildId, channelId):
