82252ff0d7fcdd470260f24b603588068c861031
Ryan Conrad <camalot@gmail.com>
2021-09-10 15:48:02 -0500

    update manifest with whats new

--- bot/cogs/lib/settings.py
+++ bot/cogs/lib/settings.py
@@ -16,14 +16,17 @@ class Settings:
                 self.__dict__.update(json.load(json_file))
         except Exception as e:
             print(e, file=sys.stderr)
-        self.db_path = utils.dict_get(os.environ, 'VCB_DB_PATH', default_value = 'voice.db')
         self.db_url = utils.dict_get(os.environ, "VCB_MONGODB_URL", default_value="")
 
+        self.bot_owner = utils.dict_get(os.environ, 'BOT_OWNER', default_value= '262031734260891648')
+        self.log_level = utils.dict_get(os.environ, 'LOG_LEVEL', default_value = 'DEBUG')
+
+        # DEPRECATED
+        self.db_path = utils.dict_get(os.environ, 'VCB_DB_PATH', default_value = 'voice.db')
         self.admin_roles = utils.dict_get(os.environ, 'ADMIN_ROLES', default_value = 'Admin').split(',')
         self.admin_users = utils.dict_get(os.environ, 'ADMIN_USERS', default_value = '').split(' ')
         self.default_role = utils.dict_get(os.environ, 'DEFAULT_ROLE', default_value= '@everyone')
-        self.bot_owner = utils.dict_get(os.environ, 'BOT_OWNER', default_value= '262031734260891648')
-        self.log_level = utils.dict_get(os.environ, 'LOG_LEVEL', default_value = 'DEBUG')
+
 
 class GuildCategorySettings:
     def __init__(self, guildId, categoryId, channelLimit, channelLocked, bitrate, defaultRole):
